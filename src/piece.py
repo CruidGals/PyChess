1456b35b6f54fd57e1d148803316db9c71a8f4c4
CruidGals <kchiem30@gmail.com>
2023-02-25 20:43:13 -0500

    Add icons for pieces

---
+++ src/piece.py
@@ -0,0 +1,30 @@
+import pygame
+
+WHITE = 0
+BLACK = 1
+
+class Pawn:
+    def __init__(self, color) -> None:
+        self.color = color
+
+class Knight:
+    def __init__(self, color) -> None:
+        self.color = color
+
+class Bishop:
+    def __init__(self, color) -> None:
+        self.color = color
+
+class Rook:
+    def __init__(self, color) -> None:
+        self.color = color
+
+class Queen:
+    def __init__(self, color) -> None:
+        self.color = color
+
+class King:
+    def __init__(self, color) -> None:
+        self.color = color
+
+
