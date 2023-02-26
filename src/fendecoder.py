04fdfcb7aca1375d35a7745cff3bf7047e41be7f
CruidGals <kchiem30@gmail.com>
2023-02-26 16:39:38 -0500

    Add piece placement decoder

--- src/fendecoder.py
+++ src/fendecoder.py
@@ -1,4 +1,4 @@
-from piece import Piece
+from board import Piece
 
 class FenDecoder:
 
@@ -7,13 +7,31 @@ class FenDecoder:
 
         #Make piece array
         piece_placements = fields[0].split('/')
-        
+        self.piece_placement = []
+        for pieces in piece_placements:
+            row = []
+            for piece in pieces:
+                if piece.isnumeric():
+                    row.append(['' for i in range(int(piece))])
+                    continue
+                
+                color = Piece.WHITE if piece.isupper() else Piece.BLACK
+
+                piece = piece.lower()
+                if piece == 'p': row.append((Piece.PAWN, color))
+                elif piece == 'n': row.append((Piece.KNIGHT, color))
+                elif piece == 'b': row.append((Piece.BISHOP, color))
+                elif piece == 'r': row.append((Piece.ROOK, color))
+                elif piece == 'q': row.append((Piece.QUEEN, color))
+                elif piece == 'k': row.append((Piece.KING, color))
+            
+            self.piece_placement.append(row)
 
 
         self.side_to_move = fields[1]
         self.castling_ability = fields[2]
         self.en_passant_square = fields[3]
-        self.half_move_counter = fields[4]
-        self.full_move_counter = fields[5]
+        self.half_move_counter = int(fields[4])
+        self.full_move_counter = int(fields[5])
 
-        print(self.piece_placements)
+        print(self.piece_placement)
