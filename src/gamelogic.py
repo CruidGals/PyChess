64b62459ffdfb09a2b6265a541cb96d15ab074bc
CruidGals <kchiem30@gmail.com>
2023-02-27 22:15:05 -0500

    Add piece moves function

--- src/gamelogic.py
+++ src/gamelogic.py
@@ -7,20 +7,41 @@ class GameLogic:
         self.piece_placement = piece_placement
     
     #Piece Moves -----------------------------------------
-    def pawn_moves(self, board):
-        pass
+    def pawn_moves(self, piece: Piece):
+        row = np.argwhere(self.piece_placement == piece)[0][0]
+        col = np.argwhere(self.piece_placement == piece)[0][1]
+        range_of_motion = []
+        direction = 1 if piece.color == Piece.BLACK else -1
+
+        for i in range(-1, 2):
+            if not (0 <= (row + direction) < 8) or not (0 <= (col + i) < 8):
+                continue
+            
+            if i == 0 and self.piece_placement[row + direction][col].piece == Piece.NO_PIECE:
+                range_of_motion.append(self.piece_placement[row + direction][col].attached_square)
+
+                #Checking double move:
+                if (row == 1 and piece.color == Piece.BLACK) or (row == 6 and piece.color == Piece.WHITE) and self.piece_placement[row + (2 * direction)][col].piece == Piece.NO_PIECE:
+                    range_of_motion.append(self.piece_placement[row + (2 * direction)][col].attached_square)
+            elif self.piece_placement[row + direction][col + i].color == Piece.opposite_color(piece.color):
+                range_of_motion.append(self.piece_placement[row + direction][col + i].attached_square)
+        
+        return range_of_motion
     
-    def knight_moves(self, board):
+    def knight_moves(self, piece):
         pass
 
-    def bishop_moves(self, board):
+    def bishop_moves(self, piece):
         pass
 
-    def rook_moves(self, board):
+    def rook_moves(self, piece):
         pass
 
-    def queen_moves(self, board):
+    def queen_moves(self, piece):
         pass
 
-    def king_moves(self, board):
+    def king_moves(self, piece):
+        pass
+
+    def is_check(self) -> bool:
         pass
