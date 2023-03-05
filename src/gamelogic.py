9cf9a0b0363c8633dc7a94d89831f208a49e8e02
CruidGals <kchiem30@gmail.com>
2023-03-05 18:34:22 -0500

    Implement checked_squares function

--- src/gamelogic.py
+++ src/gamelogic.py
@@ -1,10 +1,17 @@
 import pygame
+from pygame import Vector2
 import numpy as np
 from board import Piece
 
 class GameLogic:
     def __init__(self, piece_placement):
         self.piece_placement = piece_placement
+
+        #Noting king positions
+        for piece in piece_placement.ravel():
+            if piece.piece == Piece.KING:
+                if piece.color == Piece.WHITE: self.white_king = piece
+                else: self.black_king = piece
     
     #Piece Moves -----------------------------------------
     def pawn_moves(self, piece: Piece):
@@ -14,7 +21,7 @@ class GameLogic:
         direction = 1 if piece.color == Piece.BLACK else -1
 
         for i in range(-1, 2):
-            if not (0 <= (row + direction) < 8) or not (0 <= (col + i) < 8):
+            if not self.within_bounds(row + direction, col + i):
                 continue
             
             if i == 0 and self.piece_placement[row + direction][col].piece == Piece.NO_PIECE:
@@ -43,5 +50,76 @@ class GameLogic:
     def king_moves(self, piece):
         pass
 
-    def is_check(self) -> bool:
-        pass
+    def checked_squares(self, king: Piece):
+        color = king.color
+        row = np.argwhere(self.piece_placement == king)[0][0]
+        col = np.argwhere(self.piece_placement == king)[0][1]
+        checking_pieces = []
+
+        #Check vertical: negative means up, positive means down
+        for direction in range(-1, 2, 2):
+            for i in range(direction, direction * 8):
+                if not self.within_bounds(row + i, col): break
+
+                targeted_piece = self.piece_placement[row + i][col]
+
+                if targeted_piece.color == color:
+                    break
+                elif targeted_piece.color == Piece.opposite_color(color):
+                    if (targeted_piece.piece == Piece.ROOK) or (targeted_piece.piece == Piece.QUEEN):
+                        checking_pieces.append(targeted_piece)
+                    break
+        
+        #Check Horizontal and Diagonals: negative means left, postive means right
+        #Starts top left diagonal. ends bottom right diagonal
+        # (Remember, row is accessed from top to bottom, col is accessed from left to right)
+        for x_dir in range(-1, 2, 2):
+            for y_dir in range(-1, 2):
+                for i in range(1, 8):
+                    dx = i * x_dir
+                    dy = i * y_dir
+
+                    if not self.within_bounds(row + dy, col + dx): break
+
+                    targeted_piece = self.piece_placement[row + dy][col + dx]
+
+                    if targeted_piece.color == color:
+                        break
+                    elif targeted_piece.color == Piece.opposite_color(color):
+                        #Check Pawn
+                        if dy == -1 and color == Piece.WHITE:
+                            checking_pieces.append(targeted_piece)
+                            break          
+                        elif dy == 1 and color == Piece.BLACK:
+                            checking_pieces.append(targeted_piece)
+                            break
+
+                        #Check Rook
+                        if dy == 0 and targeted_piece.piece == Piece.ROOK:
+                            checking_pieces.apend(targeted_piece)
+
+                        if (targeted_piece.piece == Piece.BISHOP) or (targeted_piece.piece == Piece.QUEEN):
+                            checking_pieces.append(targeted_piece)
+                        break
+        
+        #Check for knights: go on each diagnoal, then check outer sides of each diagonal
+        for x_dir in range(-1, 2, 2):
+            for y_dir in range(-1, 2, 2):
+                if self.within_bounds(row + y_dir * 2, col + x_dir):
+                    targeted_piece = self.piece_placement[row + y_dir * 2][col + x_dir]
+                    if targeted_piece.color == Piece.opposite_color(color) and targeted_piece.piece == Piece.KNIGHT:
+                        checking_pieces.append(targeted_piece)
+                
+                if self.within_bounds(row + y_dir, col + x_dir * 2):
+                    targeted_piece = self.piece_placement[row + y_dir][col + x_dir * 2]
+                    if targeted_piece.color == Piece.opposite_color(color) and targeted_piece.piece == Piece.KNIGHT:
+                        checking_pieces.append(targeted_piece)
+        
+        return checking_pieces
+                    
+    
+    def within_bounds(self, x, y) -> bool:
+        if x < 0 or x > 7 or y < 0 or y > 7:
+            return False
+        else:
+            return True
